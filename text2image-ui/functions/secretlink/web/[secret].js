f0e18084e7581852df1ca7e2af5aa698e317cbf8
Vincent To <vincent_to@139.com>
2024-08-15 02:22:12 +0800

    Update function 'secretlink/web/[secret].js' to improve web UI 🍭

--- text2image-ui/functions/secretlink/web/[secret].js
+++ text2image-ui/functions/secretlink/web/[secret].js
@@ -7,43 +7,52 @@ export async function onRequestGet(context) {
     message = decodeURIComponent(message);
   }
   try {
-    const output_html = `<!DOCTYPE html>
+    const output_html = `
+<!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.1/css/bulma.min.css">
-    <style>
-      body {
-        background: #8c4af0;
-        height: 100vh;
-      }
-    </style>
-  </head>
-  <body class="p-3">
-    <section class="section pt-6">
-      <h1 class="title has-text-light">Unlock Secret Link</h1>
-      <div class="block is-flex is-justify-content-start">
-        <form action="/secretlink/redirect" method="post">
-          <input type="hidden" name="secret" value="${context.params.secret}">
-          <div class="field has-addons">
-            <div class="control">
-              <input class="input is-large" type="text" name="passcode" placeholder="your passcode" size="12" maxlength="12" required>
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.1/css/bulma.min.css">
+      <style>
+         body {
+         background: #6c3cd9;
+         }
+         .upper-div-height {
+         height: 55vh;
+         }
+         .lower-div-height {
+         height: 45vh;
+         }
+      </style>
+   </head>
+   <body>
+      <div class="is-flex is-justify-content-center is-align-items-end upper-div-height">
+         <div>
+            <div class="title has-text-light">Unlock Secret Link</div>
+            <div class="block is-flex is-justify-content-start">
+               <form action="/secretlink/redirect" method="post">
+                  <input type="hidden" name="secret" value="${context.params.secret}">
+                  <div class="field has-addons">
+                     <div class="control">
+                        <input class="input is-large" type="text" name="passcode" placeholder="your passcode" size="12" maxlength="12" required>
+                     </div>
+                     <div class="control">
+                        <button class="button is-primary is-light is-large" type="submit">
+                        <span class="icon">
+                        <img src="/image/unlock.svg" />
+                        </span>
+                        </button>
+                     </div>
+                  </div>
+               </form>
             </div>
-            <div class="control">
-              <button class="button is-primary is-light is-large" type="submit">
-                <span class="icon">
-                  <img src="/image/unlock.svg" />
-                </span>
-              </button>
-            </div>
-          </div>
-        </form>
+         </div>
       </div>
-      <div class="block has-text-danger-80 is-size-6">${message}</div>
-    </section>
-  </body>
-</html>`;
+      <div class="p-3 has-text-centered has-text-warning-75 is-size-5 lower-div-height">${message}</div>
+   </body>
+</html>
+`;
     return new Response(output_html, {
       headers: {
         'Content-Type': 'text/html; charset=UTF-8'
